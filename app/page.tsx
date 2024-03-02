dfd90946e184daa7931504595f11b2f7756542f0
Steve Brand <thatfanficguy@protonmail.com>
2024-03-02 17:45:10 +0100

    Let there be light

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Playfield from "./layout/Playfield";
+import Sidebar from "./layout/Sidebar";
+
+export default function Home() {
+	return (
+		<>
+			<Sidebar />
+			<Playfield />
+		</>
+	);
+}
