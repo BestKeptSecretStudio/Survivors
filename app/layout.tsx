8932f4eb29bef896944f514623ceacaaceda5f70
Steve Brand <thatfanficguy@protonmail.com>
2024-03-03 01:39:17 +0100

    Formatting

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,18 +4,18 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Survivors",
-  description: "How will you survive the apocalypse?",
+	title: "Survivors",
+	description: "How will you survive the apocalypse?",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={inter.className}>{children}</body>
+		</html>
+	);
 }
