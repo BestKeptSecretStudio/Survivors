e96b3be2be22441bd9a34d592728433cd2a1590d
Steve Brand <thatfanficguy@protonmail.com>
2024-05-23 08:33:02 +0200

    Reset development

--- app/layout/Playfield.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const StyledPlayfield = styled.main`
-	display: flex;
-`;
-
-export default function Playfield() {
-	return <StyledPlayfield>Main</StyledPlayfield>;
-}
--- app/types/utilities.ts
+++ app/types/utilities.ts
@@ -1,3 +1,23 @@
+// * source for the `Increment` type:
+// > https://stackoverflow.com/a/76233031
+
+type _IncrementDigit = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+type Digit = _IncrementDigit[number];
+
+type _IncrementIntermediate<T extends string> = T extends `${infer F}${Digit}`
+	? T extends `${F}${infer L extends Digit}`
+		? `${L extends 9 ? _IncrementIntermediate<F> : F}${_IncrementDigit[L]}`
+		: never
+	: 1;
+
+type Increment<T extends number> = number extends T
+	? number
+	: `${T}` extends `${string}${"." | "+" | "-" | "e"}${string}`
+	? number
+	: _IncrementIntermediate<`${T}`> extends `${infer N extends number}`
+	? N
+	: never;
+
 type Enumerate<
 	N extends number,
 	Acc extends number[] = []
@@ -5,10 +25,9 @@ type Enumerate<
 	? Acc[number]
 	: Enumerate<N, [...Acc, Acc["length"]]>;
 
-type IntRange<F extends number, T extends number> = Exclude<
-	Enumerate<T>,
+export type Range<F extends number, T extends number> = Exclude<
+	Enumerate<Increment<T>>,
 	Enumerate<F>
 >;
 
-// * 0 to 100 inclusive, since <F> alone makes the range exclusive
-export type Percentile = IntRange<0, 101>;
+export type Percentile = Range<0, 100>;
