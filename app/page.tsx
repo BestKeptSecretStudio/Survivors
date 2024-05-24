09c684da9f0d1756c23369632f65087a1a7fc195
Steve Brand <thatfanficguy@protonmail.com>
2024-05-24 12:53:49 +0200

    Update page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,31 @@
 "use client";
 import { Provider } from "react-redux";
-import TopBar from "./layout/TopBar";
 import { store } from "./store";
+import styled from "@emotion/styled";
+
+import Time from "@components/Time";
+import ActionsBar from "@layout/ActionsMenu";
+import ResourceBar from "@layout/ResourceBar";
+
+const Wrapper = styled.main`
+	min-height: 100%;
+
+	display: grid;
+	grid-template: 1rem auto / clamp(8em, 25vw, 16em) auto;
+	gap: 1rem;
+
+	padding: 0.5rem;
+`;
 
 export default function Home() {
 	return (
 		<Provider store={store}>
-			<TopBar />
+			<Wrapper>
+				<Time />
+				<ResourceBar />
+				<ActionsBar />
+				<div>Playfield</div>
+			</Wrapper>
 		</Provider>
 	);
 }
