d65ad08665bc5d9c3e357be821bc4d250b8189a4
Steve Brand <thatfanficguy@protonmail.com>
2025-06-16 23:12:18 +0200

    Update Home.tsx

--- app/layout/screens/Home.tsx
+++ app/layout/screens/Home.tsx
@@ -1,5 +1,21 @@
-function HomeScreen() {
-	return <>Home, sweet home!</>;
-}
+import Actions, { Action, Heading } from "@/components/ActionsMenu";
+
+const HomeScreen = () => {
+	return (
+		<div className="flex flex-col">
+			<p>Your haven is secure.</p>
+			<p>The weather is currently clear. It's going to rain lightly soon.</p>
+			<p>The surroundings are quiet.</p>
+			<Actions>
+				<Actions.Section>
+					<Heading>Home</Heading>
+					<Actions.List>
+						<Action>Look around</Action>
+					</Actions.List>
+				</Actions.Section>
+			</Actions>
+		</div>
+	);
+};
 
 export default HomeScreen;
