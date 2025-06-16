b3fae687b74c3744198bc7847a02f3f2bef1c9ba
Steve Brand <thatfanficguy@protonmail.com>
2025-06-16 21:51:27 +0200

    Update TimeSlice

--- app/state/TimeSlice.ts
+++ app/state/TimeSlice.ts
@@ -1,5 +1,5 @@
 import { persistentAtom } from "@nanostores/persistent";
-import dayjs, { type Dayjs } from "dayjs";
+import dayjs from "dayjs";
 import { atom, computed } from "nanostores";
 import timestring, { type Options } from "timestring";
 
@@ -23,8 +23,8 @@ export const $currentDayjs = computed($currentTime, (timeStr) =>
 	dayjs(timeStr),
 );
 
-export const $startingDayjs = computed($startingDate, (startingStr) =>
-	dayjs(startingStr),
+export const $startingDayjs = computed($startingDate, (startingDayString) =>
+	dayjs(startingDayString),
 );
 
 export const $timeDifference = computed(
@@ -33,7 +33,7 @@ export const $timeDifference = computed(
 );
 
 export const $daysSurvived = computed($timeDifference, (diff) =>
-	Math.floor(diff / (1000 * 60 * 60 * 24)),
+	Math.floor(diff / 86_400_000 /* 1000 ms * 60 s * 60 m * 24 h */),
 );
 
 export const $formattedTime = computed($currentDayjs, (dayjs) =>
@@ -45,7 +45,8 @@ export const $formattedDate = computed($currentDayjs, (dayjs) =>
 );
 
 export const advanceTime = (amount: string) => {
+	const currentTime = $currentTime.get();
 	const seconds = timestring(amount, undefined, timestringOptions);
-	const newTime = dayjs($currentTime.get()).add(seconds, "seconds");
+	const newTime = dayjs(currentTime).add(seconds, "seconds");
 	$currentTime.set(newTime.format());
 };
